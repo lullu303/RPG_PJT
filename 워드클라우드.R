f05cc968ee541f43e44914fe4cbb3cc6df834046
LeeSeunghwan <126847274+WaringWaring@users.noreply.github.com>
2023-05-18 08:50:34 +0900

    Add files via upload

--- 워드클라우드.R
+++ 워드클라우드.R
@@ -1,18 +1,17 @@
 
-# 전체 url
-#url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%BD%98%EC%86%94%EA%B2%8C%EC%9E%84&sort=0&photo=0&field=0&pd=13&ds=2023.02.10&de=2023.05.11&cluster_rank=65&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:3m,a:all&start=1"
-
-
 url1 <- "https://search.naver.com/search.naver?where=news&sm=tab_pge&query="
-keyword <- "콘솔게임"
+keyword <- "콘솔게임"          
+
+
 url2 <- "%EC%BD%98%EC%86%94%EA%B2%8C%EC%9E%84&sort=0&photo=0&field=0&pd=13&"
-start_date <- "ds=2023.02.10&"
+start_date <- "ds=2023.02.10&"     #기사가 올라온 날짜 범위
 end_date <- "de=2023.05.11&"
+
 url3 <- "cluster_rank=65&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:3m,a:all&start=1"
-PAGE <- seq(from=1, by=10, length.out=50)
+PAGE <- seq(from=1, by=10, length.out=50)     # 기사 페이지 범위
 
 
-#웹 크롤링
+#크롤링
 library(rvest)
 library(httr)
 
@@ -56,7 +55,7 @@ result_tb <- result %>%
 result_tb
 
 
-#불용어 삭제
+#안쓸 단어 삭제
 st_word <- tibble(word=c('비트코','삼국블레이드','1분기','롯데월드','체험존','어트랙션','삼성전자'))
 
 result_tb <- result_tb %>%
